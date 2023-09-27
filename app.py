7bb5b48339839c67f0f724cd36621a68f4ec54ca
Nil Das <dasnil500@gmail.com>
2023-09-28 00:16:46 +0530

    added inference api

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 import streamlit as st
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-import torch
 import re
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/dasnil500/end-to-end-am"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
 # Load the pre-trained model and tokenizer
-checkpoint_path = "dasnil500/end-to-end-am"
-model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint_path)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = model.to(device)
-tokenizer = AutoTokenizer.from_pretrained('t5-base', model_max_length=512)
+# checkpoint_path = "Frontend/"
+# model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint_path)
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# model = model.to(device)
+# tokenizer = AutoTokenizer.from_pretrained('t5-base', model_max_length=512)
 
 # I do not understand the reason behind the protest. Farm law will greatly benefit small & marginal farmers of our country.Because, currently they cannot bargain for their produce and get a better price and cannot invest in technology to improve the productivity of the farms. This law will allow farmers to sell their product outside APMC “mandis” to whoever they want and get better price for their produce. We appreciate Government’s sensitivity towards us. But Government should listen our side too. This law will dismantle the minimum support price (MSP) system. Because, over time big corporate houses will dictate terms and we will end up getting less for our crops we produce. Government should give us a legal assurance that the MSP system will continue alongside.
 def main():
@@ -21,19 +28,25 @@ def main():
     
     if st.button("Generate Output"):
         if input_sentence:
-            # Tokenize the input text
-            input_ids = tokenizer.encode(input_sentence, return_tensors="pt", max_length=512, truncation=True).to(device)
+            # # Tokenize the input text
+            # input_ids = tokenizer.encode(input_sentence, return_tensors="pt", max_length=512, truncation=True).to(device)
 
-            # Generate output using the model
-            output = model.generate(input_ids, max_length=512, num_return_sequences=1, num_beams=8)
-            generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+            # # Generate output using the model
+            # output = model.generate(input_ids, max_length=512, num_return_sequences=1, num_beams=8)
+            # generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+            output = query({
+                "inputs": input_sentence,
+                "parameters" : {"max_length": 512}
+            })
 
+            generated_text = output[0]['generated_text']
             print(generated_text)
+
             pattern = r'\[(.*?)\]'
 
             # Find all matches in the text
             matches = re.findall(pattern, generated_text, re.DOTALL)
-            # print(matches)
+            print(matches)
             # Initialize lists to store extracted data
             text1_list = []
             text2_list = []
